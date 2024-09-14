cf767abe63fc146c7069a6dd26b68ed7098327a3
oitee <oitee.codes@gmail.com>
2024-09-14 16:37:23 +0530

    Add sample route to add a new user

--- src/main/java/dev/otee/robin/model/UserRepository.java
+++ src/main/java/dev/otee/robin/model/UserRepository.java
@@ -2,6 +2,5 @@ package dev.otee.robin.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface UserRepository extends CrudRepository<Users, Integer> {
 }
--- src/main/java/dev/otee/robin/rest/MovieController.java
+++ src/main/java/dev/otee/robin/rest/MovieController.java
@@ -1,6 +1,10 @@
 package dev.otee.robin.rest;
 
+import dev.otee.robin.model.UserRepository;
+import dev.otee.robin.model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,15 @@ public class MovieController {
     @GetMapping("/")
     public String hello(){
         return "Hello World";
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/add/{name}")
+    public String welcome(@PathVariable String name){
+        Users myUser = new Users(name);
+        userRepository.save(myUser);
+        return name;
     }
 }
